2da18ba2e17dc57d8b76190a69600d2404bc0d39
Emily <emily.kirmix@gmail.com>
2024-04-25 12:10:26 +0900

    cambio de color en el fondo

--- ingreso_user.php
+++ ingreso_user.php
@@ -7,15 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.css">
     <scrip src="js/bootstrap.js"></scrip>
+    <link rel="stylesheet" href="css/ingreso_user.css">
 
     <title>Ingreso Usuario</title>
 </head>
 <body>
 
-    <div class="page-header">
+    <div class="div_header page-header">
         <div class="row">
             <div class="col-md-2"></div>
-            <div class="col-md-8 text-center"><h1>INGRESO DE USUARIO </h1></div>
+            <div class="col-md-8 text-center"><h1 class="titulo">INGRESO DE USUARIO </h1></div>
             <div class="col-md-2"></div>
         </div>
     </div>
@@ -25,17 +26,27 @@
         <div class="form-group col-md-8">
             <div class="row">
                 <form action="" method="post">
-                    <label for="user_name">Nombre de Usuario</label>
-                    <input type="text" name="user_name" placeholder="Nombre de Usuario" id="user_name" required>
 
-                    <label for="nombre">Nombre</label>
-                    <input type="text" name="nombre" placeholder="Nombre" id="nombre" required>
+                    <div class="input-group">
+                        <label for="user_name">Nombre de Usuario</label>
+                        <input type="text" name="user_name" placeholder="Nombre de Usuario" id="user_name" required>
+                    </div>
 
-                    <label for="apellido">Apellido</label>
-                    <input type="text" name="apellido" placeholder="Apellido" id="apellido" required>
+                    <div class="input-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" placeholder="Nombre" id="nombre" required>
+                    </div>
 
-                    <label for="password">Contraseña</label>
-                    <input type="password" name="password" placeholder="Contraseña" id="contraseña" required>
+                    <div class="input-group">
+                        <label for="apellido">Apellido</label>
+                        <input type="text" name="apellido" placeholder="Apellido" id="apellido" required>
+                    </div>
+
+                    <div class="input-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password" placeholder="Contraseña" id="contraseña" required>
+                    </div>
+
                 </form>
             </div>
         </div>
